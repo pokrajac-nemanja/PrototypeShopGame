f3cd22f4669054a96c5410a36f4c01a27cfadde0
nemanja.pokrajac <nemanja.pokrajac@outlook.com>
2024-01-24 17:01:25 +0100

    Added basic movement - fb002
    Basic WASD movement controls, with a LShift to run.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float baseSpeed = 5;
+    private float runModifier = 2;
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = baseSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateSpeed();
+        UpdateMovement();
+    }
+
+    private void UpdateSpeed()
+    {
+        speed = baseSpeed;
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed *= runModifier;
+        }
+    }
+
+    private void UpdateMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+        }
     }
 }
